6b2dd5d06125e741e9ae89f1a864fed1976a8439
perez.daniel <ts-daniel.perez@rakuten.com>
2019-12-12 12:34:19 +0900

    counted kanji

--- publicside/assets/frontjs.js
+++ publicside/assets/frontjs.js
@@ -14,15 +14,36 @@ function doesKanjiExist(ch){
 
 function buttonClick(){
 	event.preventDefault();
-	var kaniInput = $("#kanjiInput").val().trim();
+	var kanjiInput = $("#kanjiInput").val().trim();
 	//run function and pass text into it. 
-	if(doesKanjiExist(kaniInput)){
+	if(doesKanjiExist(kanjiInput)){
+		//count the kanji in and order by most frequent 
 		console.log("there is kanji");
+		countKanji(kanjiInput);
 	}
 	else{
-		console.log("there is no kanji.")
+		console.log("there is no kanji.");
 	}
 	
+}
+
+// creates obj of counted kanji 
+function tallupelements(obj, word){
+	if(!obj[word]){
+		obj[word] = 0; 
+	}
+	obj[word] ++;
+	return obj;
+}
+
+// return object of counted kanjis 
+function countKanji(input){
+	// turn string to array so we can get ready to count. 
+	var inputArray = input.split("");
+	var kanjiCount = {}; 
+	//count dups in array 
+	var count = inputArray.reduce(tallupelements, kanjiCount);
+	console.log(count); 
 }
 
 //button click handler. 
@@ -34,9 +55,9 @@ $(document).on("click", "#submit-kanji-button", buttonClick);
 // 1b. write funtion for button - start with console log - done 
 // 1c. find out why this didnt show up on github - done 
 
-// 2. loop through list and create list of kanji -- make sure to add duplicate 
+// 2. loop through list and create list of kanji -- make sure to add duplicate - done 
 // 2a. loop through new list and order by most frequent kanji 
-// 2b. gather count of occurence of kanji 
+// 2b. gather count of occurence of kanji - done 
 // 2c. research how to prepare data to send to wani kani api 
 
 // 3. Look API from wanki kani and see if i can make calls. 
