22bdc8fe0f88eef29f619db4d87273ced249bc19
perez.daniel <ts-daniel.perez@rakuten.com>
2019-12-19 19:09:44 +0900

    add request npm and got response from WK

--- app/routing/waniAPI.js
+++ app/routing/waniAPI.js
@@ -1,10 +1,34 @@
 // this will need to change when i push it to heroku. 
-// var WKapiKey = require('../../configkeys/congif.js');
+// make this file exist on laptop. 
+var WKkeys = require('../../configkeys/apiKeys.js');
+const request = require('request');
+var key = WKkeys.apiKeys.waniKani;
 
 module.exports = function(app) {
     app.post("/api/routes", function(req, res){
-        console.log(req);
+        testing();
         res.json("test");
         
+    });
+}
+
+const options = {
+    url: 'https://api.wanikani.com/v2/subjects',
+    method: 'GET',
+    headers: {
+        Authorization: 'Bearer ' + key
+    }
+};
+
+function testing(){
+    request(options, function(error, response, body) {
+        if (!error) {
+            let json = JSON.parse(body)
+            console.log(response);
+            console.log(body);
+        }
+        else{
+            console.log("there was an error. ");
+        }
     });
 }
