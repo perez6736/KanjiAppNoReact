53aa7e53f80af2b4efe5e550d09f345901bb466c
perez6736 <perez6736@gmail.com>
2019-12-20 00:00:38 +0900

    able to send response from wani kani to front end

--- app/routing/waniAPI.js
+++ app/routing/waniAPI.js
@@ -1,34 +1,36 @@
 // this will need to change when i push it to heroku. 
-// make this file exist on laptop. 
 var WKkeys = require('../../configkeys/apiKeys.js');
 const request = require('request');
 var key = WKkeys.apiKeys.waniKani;
 
 module.exports = function(app) {
     app.post("/api/routes", function(req, res){
-        testing();
-        res.json("test");
-        
+        // idk what im doing - // function gets kanji and sends back results to UI
+        getKanji(function (body){
+            res.json(body); 
+        })
     });
 }
 
+// stuff for wani kani api 
 const options = {
-    url: 'https://api.wanikani.com/v2/subjects',
+    url: 'https://api.wanikani.com/v2/subjects?type=kanji',
     method: 'GET',
     headers: {
         Authorization: 'Bearer ' + key
     }
 };
 
-function testing(){
+function getKanji(callback){
     request(options, function(error, response, body) {
         if (!error) {
             let json = JSON.parse(body)
-            console.log(response);
-            console.log(body);
+            callback(body); //send response through call back
         }
         else{
+            var errmsg = "error"
             console.log("there was an error. ");
+            return errmsg;
         }
     });
 }
