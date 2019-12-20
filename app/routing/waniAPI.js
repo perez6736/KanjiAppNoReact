8b77e5bb6925a76c936298059b5bf21b6b41b9c2
perez.daniel <ts-daniel.perez@rakuten.com>
2019-12-20 09:29:54 +0900

    small organization

--- app/routing/waniAPI.js
+++ app/routing/waniAPI.js
@@ -3,17 +3,26 @@ var WKkeys = require('../../configkeys/apiKeys.js');
 const request = require('request');
 var key = WKkeys.apiKeys.waniKani;
 
+// =====================================================
+// stuff for routes - backend <==> frontend
+// =====================================================
 module.exports = function(app) {
     app.post("/api/routes", function(req, res){
-        // idk what im doing - // function gets kanji and sends back results to UI
+        // idk what im doing - 
+        // function gets kanji and sends back results to UI
         getKanji(function (body){
             res.json(body); 
         })
     });
 }
 
+
+
+
+// =====================================================
 // stuff for wani kani api 
-const options = {
+// =====================================================
+const getKanjiType_WK = {
     url: 'https://api.wanikani.com/v2/subjects?type=kanji',
     method: 'GET',
     headers: {
@@ -22,7 +31,7 @@ const options = {
 };
 
 function getKanji(callback){
-    request(options, function(error, response, body) {
+    request(getKanjiType_WK, function(error, response, body) {
         if (!error) {
             let json = JSON.parse(body)
             callback(body); //send response through call back
