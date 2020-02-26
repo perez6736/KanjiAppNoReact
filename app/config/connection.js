223c8d4c6e01762289b2efcdd6e0383f1e13c1ec
perez6736 <perez6736@gmail.com>
2020-02-26 21:38:01 +0900

    added mysql finally

---
+++ app/config/connection.js
@@ -0,0 +1,28 @@
+// dependencies ===================================================== 
+var mysqlv= require('mysql');
+
+// DB connection =====================================
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host     : "localhost",
+    user     : "root",
+    password : "mysqlppw123",
+    database : "burgers_db"
+  });
+}
+
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
