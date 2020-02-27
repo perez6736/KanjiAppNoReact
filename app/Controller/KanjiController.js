9d6e7b09c377b03ca907c9193cee24c92f5339e9
perez.daniel <ts-daniel.perez@rakuten.com>
2020-02-27 09:51:21 +0900

    change name of url

--- app/Controller/KanjiController.js
+++ app/Controller/KanjiController.js
@@ -8,12 +8,12 @@ var orm = require("../Model/orm.js"); // use this to to get the data...
 // this file will manipulate the data from the database... and should respond to the view where we can use it . 
 
 // this creates a kanji in database. 
-router.post("/api/burgers", function(req, res){
+router.post("/db/kanji", function(req, res){
     console.log('create a kanji in database ')
 });
 
 // delete kanji ? - do i need this? 
-router.put("/api/kanji/:id", function(req, res) {
+router.put("/db/kanji/:id", function(req, res) {
     console.log('this will be delete?')
 });
 
